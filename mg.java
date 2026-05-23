aa6f7ff1e3fb20f7a5d772b4f7b1e8aefaaff0aa
unknown <gopikaasudhakar@gmail.com>
2026-05-23 14:45:48 +0530

    hello

--- mg.java
+++ mg.java
@@ -1,1 +1,12 @@
-import java.util.*;
+class Cart{
+    int amount=0;
+    int item=0;
+    void addToCart(int price){
+        amount+=price;
+        item++;
+    }
+    void printstatus(){
+        System.out.println("Total amount: "+amount);
+        System.out.println("Total items: "+item);
+    }
+}
